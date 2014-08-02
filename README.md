bdeb9d1db3de27562471876108f33af5cba00b1a
Anders Aagaard <aagaande@gmail.com>
2014-08-02 16:53:15 +0200

    initial import

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-scikit-helpers
+scikit_helpers
 ==============
 
-My personal tools for working with scikit/ML
+My personal tools for working with scikit/ML. For now it's only my feature building tools with scikit pipelines and pandas dataframes.
+
+	pipe = sklearn.pipeline.Pipeline([
+	    ("features", sklearn.pipeline.FeatureUnion([
+	                ("single_features", PickFeature(select_columns=["ColumnA","ColumnB"], return_sparse=True)),
+	                ("text_pipeline", FeatureNamedPipeline([
+	                    ("pick", PickFeature(select_columns=['Annonseoverskrift'], return_single_row=True)),
+	                    ("vect", sklearn.feature_extraction.text.CountVectorizer()),
+	                    ("kbest", sklearn.feature_selection.SelectKBest(sklearn.feature_selection.chi2)),
+	                	], feature_function='lambda x: np.asarray(x.named_steps["vect"].get_feature_names())[x.named_steps["kbest"].get_support()]'))
+	             ])),
+	    ("svc", sklearn.svm.LinearSVC())
+	    ])
+	pipe.fit(X, y) #X in this case is a pandas dataframe.
+	pipe.predict(X)
+
+
+Now you can easily run a gridsearch tuning parameters for everything from the svc in the bottom, kbest/the vectorizer etc. It also supports pipe.named_steps["features"].get_feature_names(). 
+
+In this example I convert the entire featureunion to sparse. Of course you can skip the return_sparse function, but then you would have to stick a ToDense() feature after SelectKBest in order for the FeatureUnion to be able to merge them.
---
+++ features.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import numpy as np
+import sklearn
+import sklearn.pipeline
+import sklearn.base
+import scipy
+import pandas as pd
+
+# <codecell>
+
+class FeatureNamedPipeline(sklearn.pipeline.Pipeline):
+    """
+    Allows generating pipelines with get_feature_names:
+    
+    sklearn.pipeline.FeatureUnion([
+            ("testa", FeatureNamedPipeline([
+                ("multiple_columns", PickFeature(select_columns=['Col1', 'Col2'], column_names=data.columns, return_single_row=False)),
+                ("csr", ToSparse()),
+            ], feature_function="lambda x: x.named_steps["multiple_columns"].select_columns")),
+
+            ("testb", FeatureNamedPipeline([
+                ("single_column", PickFeature(select_columns=['TextColumn'], column_names=data.columns, return_single_row=True)),
+                ("vect", feature_extraction.text.CountVectorizer(min_df=0.1)),
+                ("denseup", ToDense()),
+                ("csr", ToSparse()),
+            ], feature_function="lambda x: x.named_steps["vect"].vocabulary_"))
+    ])
+    
+    Of course this example doesn't make much sense. You wouldn't want to convert something to dense and then back to sparse.
+    
+    NB : due to pickle limitations (that are probably fixable) lambdas must for now be strings due to multiprocessing.
+    NB2: colunm_names are only needed if the input isn't already a pandas dataframe. Note that you need a very recent 
+         pandas and scikit in order to push a dataframe into a pipeline.
+    
+    """
+    def __init__(self, steps, feature_function=None):
+        sklearn.pipeline.Pipeline.__init__(self, steps)
+        self.feature_function = feature_function
+        
+    def get_feature_names(self):
+        return eval(self.feature_function)(self)
+
+# <codecell>
+
+class PickFeature(sklearn.base.BaseEstimator):
+    """
+    Picks features, see FeatureNamedPipeline for full example.
+    - select_columns = which columns to select
+    - column_names = column names if columns being parsed in are not already a dataframe
+    - return_single_row = returns [0] instead of [[0]]
+    - return_sparse = return sparse matrix
+    """
+    def __init__(self, select_columns=[], column_names=None, return_single_row=False, return_sparse=False):
+        self.select_columns = select_columns
+        self.column_names = column_names
+        self.return_single_row = return_single_row
+        self.return_sparse = return_sparse
+        assert len(self.select_columns) > 0
+        assert not self.return_single_row or len(self.select_columns) == 1
+    
+    def get_feature_names(self):
+        return self.select_columns
+    
+    def fit(self, items, y=None):
+        return self
+    
+    def transform(self, items):
+        if type(items) is not pd.DataFrame:
+            assert self.column_names is not None
+            items = pd.DataFrame.from_records(items, columns=self.column_names)
+        if self.return_single_row:
+            result = items[self.select_columns[0]]
+        else:
+            result = items[self.select_columns]
+        if self.return_sparse:
+            if type(result) is not pd.DataFrame:
+                result = pd.DataFrame(result)
+                
+            if sum(result.dtypes == np.object) > 0:
+                print("WARNING: Will not be able to convert this to sparse due to objects in lists!")
+                print(result.dtypes[result.dtypes == np.object])
+            result = result.to_sparse()
+        return result
+
+# <codecell>
+
+class ToDense(sklearn.base.BaseEstimator):
+    """
+    Converts data to dense, see FeatureNamedPipeline for full example.
+    """
+    
+    def fit(self, items, y=None):
+        return self
+    
+    def transform(self, items):
+        return items.todense()
+
+# <codecell>
+
+class ToSparse(sklearn.base.BaseEstimator):
+    """
+    Converts data to sparse, see FeatureNamedPipeline for full example.
+    """
+    
+    def fit(self, items, y=None):
+        return self
+    
+    def transform(self, items):
+        return scipy.sparse.csr_matrix(items)
+
+# <codecell>
+
+class ProbabilityPipeline(sklearn.pipeline.Pipeline):
+    """
+    Simply a pipeline where the end transform function calls proba/log_proba.
+    Can be used as part of a larger pipeline
+    """
+    def __init__(self, steps, log_proba=False):
+        sklearn.pipeline.Pipeline.__init__(self, steps)
+        self.log_proba = log_proba
+        
+    def get_feature_names(self):
+        return self.column_names
+    
+    def transform(self, X):
+        if self.log_proba:
+            predicted = super(ProbabilityPipeline, self).predict_log_proba(X)
+        else:
+            predicted = super(ProbabilityPipeline, self).predict_proba(X)
+        self.column_names = ["col_"+str(i) for i in range(predicted.shape[1])]
+        return predicted
+
+# <codecell>
+
+
