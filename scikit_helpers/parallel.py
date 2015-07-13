3deeb66c11b3af801ceaf99646acb3025ded7199
Anders Aagaard <aagaande@gmail.com>
2015-07-13 15:40:03 +0200

    making n_splits and n_jobs configurable in parallel.parallel

--- scikit_helpers/parallel.py
+++ scikit_helpers/parallel.py
@@ -1,19 +1,20 @@
 
 # coding: utf-8
 
-# In[95]:
+# In[99]:
 
 import pandas as pd
 import numpy as np
 from sklearn.externals import joblib
 from sklearn.externals.joblib import Parallel, delayed
+import psutil
 
 
-# In[96]:
+# In[102]:
 
-def parallel(df, function, **kwargs):
-    chunks = np.array_split(df, 20)
-    parsed = Parallel(n_jobs=-1)(delayed(test)(chunks[i], **kwargs) for i in range(len(chunks)))
+def parallel(df, function, n_splits=psutil.cpu_count() * 4, n_jobs=-1, **kwargs):
+    chunks = np.array_split(df, n_splits)
+    parsed = Parallel(n_jobs=n_jobs)(delayed(test)(chunks[i], **kwargs) for i in range(len(chunks)))
     
     if len(df.shape) == 1:
         parsed = np.hstack(parsed)
@@ -22,7 +23,7 @@ def parallel(df, function, **kwargs):
     return parsed
 
 
-# In[97]:
+# In[104]:
 
 #a = pd.DataFrame(list(range(1000)), columns=["Test"])
 #b = a["Test"]
