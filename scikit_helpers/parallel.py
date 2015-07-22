4246549ca1fb886047961dc37a169ba80c3f1136
Anders Aagaard <aagaande@gmail.com>
2015-07-22 13:54:53 +0200

    fixing parallel

--- scikit_helpers/parallel.py
+++ scikit_helpers/parallel.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[99]:
+# In[177]:
 
 import pandas as pd
 import numpy as np
@@ -10,24 +10,29 @@ from sklearn.externals.joblib import Parallel, delayed
 import psutil
 
 
-# In[102]:
+# In[178]:
 
 def parallel(df, function, n_splits=psutil.cpu_count() * 4, n_jobs=-1, **kwargs):
+    ## nb : currently only supports returning single column, can work around that with reshaping though
     chunks = np.array_split(df, n_splits)
-    parsed = Parallel(n_jobs=n_jobs)(delayed(test)(chunks[i], **kwargs) for i in range(len(chunks)))
-    
-    if len(df.shape) == 1:
-        parsed = np.hstack(parsed)
-    else:
-        parsed = np.vstack(parsed)
+    parsed = Parallel(n_jobs=n_jobs)(delayed(function)(chunks[i], **kwargs) for i in range(len(chunks)))
+    parsed = np.vstack(parsed)
     return parsed
 
 
-# In[104]:
+# In[181]:
+
+#def f(v):
+#    return v / 2
 
 #a = pd.DataFrame(list(range(1000)), columns=["Test"])
-#b = a["Test"]
-#pd.DataFrame(parallel(a, lambda v:v/2)).head()
+#a["div"] = parallel(a, f, n_jobs=-1)
+#a.head()
+
+
+# In[ ]:
+
+
 
 
 # In[ ]:
