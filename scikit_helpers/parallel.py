47b26de1e4723b05ac85ff4f3a7119a7a93d93b3
Anders Aagaard <aagaande@gmail.com>
2015-08-14 12:18:05 +0200

    adding support for groupby objects in parallel function

--- scikit_helpers/parallel.py
+++ scikit_helpers/parallel.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 import pandas as pd
 import numpy as np
@@ -9,29 +9,45 @@ import multiprocessing
 import psutil
 
 
-# In[ ]:
+# In[2]:
 
 def _apply_df(args):
     df, func, kwargs = args
     return df.apply(func, **kwargs)
 
 
-# In[ ]:
+# In[3]:
+
+def _apply_grouped_df(args):
+    grp, df, func, kwargs = args
+    r = df.apply(func, **kwargs)
+    if type(r) is pd.Series:
+        r.name = grp
+    return r
+
+
+# In[4]:
 
 def parallel(df, func, **kwargs):
-    n_jobs = kwargs.pop('n_jobs')
+    n_jobs = kwargs.pop('n_jobs') if 'n_jobs' in kwargs else 1
     if n_jobs == -1:
         n_jobs = psutil.cpu_count()
-    elif n_jobs is None:
-        n_jobs = 1
-    pool = multiprocessing.Pool(processes=n_jobs)
-    result = pool.map(_apply_df, [(d, func, kwargs)
-            for d in np.array_split(df, n_jobs)])
-    pool.close()
-    return pd.concat(list(result))
+    with multiprocessing.Pool(processes=n_jobs) as pool:
+        if type(df) is pd.core.groupby.DataFrameGroupBy:
+            result = pool.map(_apply_grouped_df, [(grpname, d, func, kwargs)
+                for grpname, d in df])
+            if len(result) == 0:
+                return result
+            if type(result[0]) is pd.Series:
+                return pd.concat(result, axis=1).T
+            return pd.concat(result)
+        else:
+            result = pool.map(_apply_df, [(d, func, kwargs)
+                for d in np.array_split(df, n_jobs)])
+            return pd.concat(list(result))
 
 
-# In[1]:
+# In[5]:
 
 #def f(v):
 #    return v / 2
@@ -42,5 +58,17 @@ def parallel(df, func, **kwargs):
 
 # In[ ]:
 
+#def grpfunc(v):
+#    return v.sum()
 
+
+# In[6]:
+
+#a["FakeGroup"] = a["Test"].apply(lambda v:int(v % 2 == 0))
+#a.groupby("FakeGroup").apply(grpfunc)
+
+
+# In[8]:
+
+#parallel(a.groupby("FakeGroup"), grpfunc, n_jobs=-1)
 
